018fca8877cbd1ba6283b36c0fad451dda46b5c4
leo <lutianming1005@gmail.com>
2013-01-21 00:10:46 +0800

    can load json model, problem with texture

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-var camera, scene, renderer, controls;
+var camera, scene, renderer, controls, loader;
 var geometry, material, mesh;
+var meshs = [];
+
+loader = new THREE.JSONLoader();
 
 function start() {
     if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
-    initWebGL()
-    animate()
+    initWebGL();
+    animate();
 }
 function initWebGL() {
 
@@ -31,7 +34,11 @@ function initWebGL() {
     material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 
     mesh = new THREE.Mesh( geometry, material );
+    meshs.push(mesh);
     scene.add( mesh );
+
+    //lights
+    scene.add(new THREE.AmbientLight(0xcccccc));
 
     params = {};
     canvas = document.getElementById("canvas");
@@ -39,12 +46,10 @@ function initWebGL() {
     renderer = new THREE.WebGLRenderer(params);
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-//    document.body.appendChild( renderer.domElement );
     window.addEventListener( 'resize', onWindowResize, false );
 }
 
 function onWindowResize() {
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
@@ -59,9 +64,50 @@ function animate() {
 
     // note: three.js includes requestAnimationFrame shim
     requestAnimationFrame( animate );
-    controls.update();
+    render();
+    if(controls.enabled){
+        controls.update();
+    }
 }
 
 function render() {
     renderer.render(scene, camera);
+}
+
+function upload() {
+    files = document.getElementById("files");
+    path = files.text;
+    url = window.URL.createObjectURL(files.files[0]);
+//    load("models/monster.js");
+    load(url);
+}
+
+function load(model, textureURL) {
+    if(typeof textureURL == "undefined"){
+        textureURL = null;
+    }
+    if(textureURL != null){
+        var texture = new THREE.Texture();
+        var imageLoader = new THREE.ImageLoader();
+        imageLoader.addEventListener('load', function(event){
+            texture.image = event.content;
+            texture.needsUpdate = true;
+        });
+        imageLoader.load(textureURL);
+    }
+
+    loader.load(model,
+                function(geometry, materials) {
+                    material = new THREE.MeshFaceMaterial(materials);
+                    mesh = new THREE.Mesh(geometry, material);
+
+                    //clean old meshs
+                    for(var i = 0; i < meshs.length; i++){
+                        m = meshs.pop();
+                        scene.remove(m);
+                    }
+
+                    meshs.push(mesh);
+                    scene.add(mesh);
+                });
 }
