a5211aed6efa47bad66b0a9a41f02d1dd9bdb763
leo <lutianming1005@gmail.com>
2013-03-11 21:30:26 +0800

    add room corner

--- js/2D.js
+++ js/2D.js
@@ -6,8 +6,9 @@ var OBJS = {
 var CMDS = {
     normal: 1,
     add_room: 2,
-    add_door_window: 3,
-    add_furniture: 4
+    add_door: 3,
+    add_window: 4,
+    add_furniture: 5
 };
 
 var g_2d = {
@@ -18,8 +19,8 @@ var g_2d = {
     selected_obj: null,
 };
 
-var rooms = [];
-var furnitures = [];
+g_2d.house = null;
+g_2d.furnitures = [];
 
 function init(){
     g_2d.stage = new Kinetic.Stage({
@@ -105,9 +106,13 @@ function onCanvasMouseDown(event){
 
         group.add(g_2d.current_obj);
         var len = points.length;
+        group.corners = [];
+        for(var i = 0; i < len; i++){
+            var corner = create_corner(points[i], group);
+            group.corners.push(corner);
+        }
         for(var i = 0; i < len; i++){
             var wall = create_wall([points[i], points[(i+1)%len]], i, group);
-
         }
 
         var walls = group.walls;
@@ -118,6 +123,9 @@ function onCanvasMouseDown(event){
         g_2d.layer.add(group);
         g_2d.layer.draw();
 
+        if(g_2d.house == null){
+            g_2d.house = group;
+        }
     }else if(g_2d.current_cmd == CMDS.add_furniture){
         var group = new Kinetic.Group({
             name: 'funiture',
@@ -133,24 +141,33 @@ function onCanvasMouseDown(event){
         });
         group.add(g_2d.current_obj);
         //4 anchors
-        var pos = g_2d.current_obj.getAbsolutePosition();
+        var p = g_2d.current_obj.getAbsolutePosition();
         var height = g_2d.current_obj.getHeight();
         var width = g_2d.current_obj.getWidth();
         var w = 10;
-        var topleft = create_anchor(pos.x, pos.y,
+        var topleft = create_anchor(p.x, p.y,
                                     -w, -w,
                                     'topleft', group);
-        var topright = create_anchor(pos.x+width, pos.y,
+        var topright = create_anchor(p.x+width, p.y,
                                      w, -w,
                                      'topright', group);
-        var bottomright = create_anchor(pos.x+width, pos.y+height,
+        var bottomright = create_anchor(p.x+width, p.y+height,
                                         w, w,
                                         'bottomright', group);
-        var bottomleft = create_anchor(pos.x, pos.y+height,
+        var bottomleft = create_anchor(p.x, p.y+height,
                                        -w, w,
                                        'bottomleft', group);
 
         g_2d.layer.add(group);
+        g_2d.layer.draw();
+    }else if(g_2d.current_cmd == CMDS.add_door){
+        console.log('add door');
+        if(g_2d.house == null){
+            console.log('null house');
+            return;
+        }
+
+        create_door(pos.x, pos.y, 90, g_2d.house);
         g_2d.layer.draw();
     }
 
@@ -194,218 +211,14 @@ function onCanvasMouseMove(event){
         g_2d.current_obj.setPoints(points);
 
         update_walls(g_2d.current_obj);
+        update_corners(g_2d.current_obj);
         g_2d.layer.draw();
     }
 
 }
 
-function update_walls(room){
-    var group = room.getParent();
-
-    var walls = group.walls;
-
-    var points = room.getPoints();
-    var len = points.length;
-    for(var i = 0; i < len; i++){
-        walls[i].setPoints([points[i], points[(i+1)%len]]);
-        walls[i].moveToTop();
-    }
-}
-
-function update_room(wall){
-    var group = wall.getParent();
-    var room = group.get('.floor')[0];
-
-    var left = wall.left;
-    var right = wall.right;
-
-    var position = wall.getPosition();
-
-    var deltax = position.x - wall.pos.x;
-    var deltay = position.y - wall.pos.y;
-
-    var points = wall.getPoints();
-
-    points = left.getPoints();
-    points[1].x += deltax;
-    points[1].y += deltay;
-    left.setPoints(points);
-
-    points = right.getPoints();
-    points[0].x += deltax;
-    points[0].y += deltay;
-    right.setPoints(points);
 
 
-    wall.pos = wall.getPosition();
-    wall.setPosition(0, 0);
-}
-
-function update_furniture(anchor){
-    var group = anchor.getParent();
-
-    var topleft = group.get('.topleft')[0];
-    var topright = group.get('.topright')[0];
-    var bottomright = group.get('.bottomright')[0];
-    var bottomleft = group.get('.bottomleft')[0];
-    var obj = group.get('.obj')[0];
-
-    var x = anchor.getX();
-    var y = anchor.getY();
-
-    switch(anchor.getName()){
-    case 'topleft':
-        topright.setY(y);
-        bottomleft.setX(x);
-        break;
-    case 'topright':
-        topleft.setY(y);
-        bottomright.setX(x);
-        break;
-    case 'bottomright':
-        topright.setX(x);
-        bottomleft.setY(y);
-        break;
-    case 'bottomleft':
-        topleft.setX(x);
-        bottomright.setY(y);
-        break;
-    default:
-        break;
-    }
-
-    obj.setPosition(topleft.getPosition());
-    var width = topright.getX() - topleft.getX();
-    var height = bottomleft.getY() - topleft.getY();
-    if (width && height){
-        obj.setSize(width, height);
-    }
-}
-
-function hide_anchors(group){
-    var topleft = group.get('.topleft')[0];
-    var topright = group.get('.topright')[0];
-    var bottomright = group.get('.bottomright')[0];
-    var bottomleft = group.get('.bottomleft')[0];
-
-    topleft.hide();
-    topright.hide();
-    bottomright.hide();
-    bottomleft.hide();
-}
-
-function show_anchors(group){
-    var topleft = group.get('.topleft')[0];
-    var topright = group.get('.topright')[0];
-    var bottomright = group.get('.bottomright')[0];
-    var bottomleft = group.get('.bottomleft')[0];
-
-    topleft.show();
-    topright.show();
-    bottomright.show();
-    bottomleft.show();
-}
-
-function create_wall(points, index, group){
-    function vertical(pos){
-        return {
-            x: this.getAbsolutePosition().x,
-            y: pos.y
-        }
-    }
-    function horizontal(pos){
-        return {
-            x: pos.x,
-            y: this.getAbsolutePosition().y
-        }
-    }
-    var wall = new Kinetic.Line({
-        points: points,
-        stroke: 'black',
-        strokeWidth: 10,
-        lineCap: 'square',
-        draggable: true,
-        dragOnTop: false
-    });
-    if(points[0].x == points[1].x){
-        wall.setDragBoundFunc(horizontal);
-    }else{
-        wall.setDragBoundFunc(vertical);
-    }
-    wall.index = index;
-    wall.on('dragstart', function(){
-        this.pos = this.getPosition();
-    });
-    wall.on('dragmove', function(){
-        update_room(wall);
-        var layer = wall.getLayer();
-        layer.draw();
-    });
-    wall.on('dragend', function(){
-
-    });
-    wall.on('mousedown touchstart', function(){
-        console.log(this.getPosition());
-        console.log(this.getPoints());
-    });
-    wall.on('mouseover', function(){
-        var layer = this.getLayer();
-        this.moveToTop();
-        this.setStroke('blue');
-        layer.draw();
-    });
-    wall.on('mouseout', function(){
-        var layer = this.getLayer();
-        this.setStroke('black');
-        layer.draw();
-    });
-
-    if(group.walls == null){
-        group.walls = [];
-    }
-    group.walls.push(wall);
-    group.add(wall);
-}
-
-function create_anchor(x, y, width, height, name, group){
-    var anchor = new Kinetic.Rect({
-        name: name,
-        x: x,
-        y: y,
-        width: width,
-        height: height,
-        fill: 'black',
-        draggable: true,
-        dragOnTop: false
-    });
-    anchor.on('dragmove', function() {
-        update_furniture(this);
-        var layer = this.getLayer();
-        layer.draw();
-    });
-    anchor.on('mousedown touchstart', function() {
-//        group.setDraggable(false);
-        this.moveToTop();
-    });
-    anchor.on('dragend', function() {
-        var layer = this.getLayer();
-//        group.setDraggable(true);
-        layer.draw();
-    });
-    // add hover styling
-    anchor.on('mouseover', function() {
-        var layer = this.getLayer();
-        document.body.style.cursor = 'pointer';
-        layer.draw();
-    });
-    anchor.on('mouseout', function() {
-        var layer = this.getLayer();
-        document.body.style.cursor = 'default';
-        layer.draw();
-    });
-    group.add(anchor);
-    return anchor;
-}
 function scale(delta){
     g_2d.stage.setScale(g_2d.stage.getScale().x + delta);
     g_2d.stage.draw();
