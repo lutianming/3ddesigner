bfb4a729bf45090ec8df48ad3a2bdf4d1f5e9a0b
leo <lutianming1005@gmail.com>
2013-04-16 23:48:10 +0800

    add rotate anchors

--- js/2Dfurniture.js
+++ js/2Dfurniture.js
@@ -1,6 +1,7 @@
 function Furniture(pos){
     Kinetic.Group.call(this, {
         name: 'furniture_group',
+        offset: [25, 25],
         draggable: true
     });
     this.type = 'furniture';
@@ -30,6 +31,26 @@ function Furniture(pos){
     this.bottomleft = create_anchor(pos.x, pos.y+height,
                                    -w, w,
                                    'bottomleft', this);
+
+    var p_center = function(p1, p2){
+        var x = (p1.x + p2.x) / 2;
+        var y = (p1.y + p2.y) / 2;
+        return {x: x, y: y};
+    };
+
+    var ptop = p_center(this.topleft.getPosition(),
+                        this.topright.getPosition());
+    var pright = p_center(this.topright.getPosition(),
+                          this.bottomright.getPosition());
+    var pbottom = p_center(this.bottomleft.getPosition(),
+                           this.bottomright.getPosition());
+    var pleft = p_center(this.topleft.getPosition(),
+                         this.bottomleft.getPosition());
+
+    this.top = create_rotate_anchor(ptop.x, ptop.y, 'top', this);
+    this.right = create_rotate_anchor(pright.x, pright.y, 'right', this);
+    this.bottom = create_rotate_anchor(pbottom.x, pbottom.y, 'bottom', this);
+    this.left = create_rotate_anchor(pleft.x, pleft.y, 'left', this);
 }
 
 Furniture.prototype = Object.create(Kinetic.Group.prototype, {
@@ -39,6 +60,11 @@ Furniture.prototype = Object.create(Kinetic.Group.prototype, {
             this.topright.hide();
             this.bottomright.hide();
             this.bottomleft.hide();
+
+            this.top.hide();
+            this.right.hide();
+            this.bottom.hide();
+            this.left.hide();
             this.draw();
         }
     },
@@ -48,6 +74,11 @@ Furniture.prototype = Object.create(Kinetic.Group.prototype, {
             this.topright.show();
             this.bottomright.show();
             this.bottomleft.show();
+
+            this.top.show();
+            this.right.show();
+            this.bottom.show();
+            this.left.show();
             this.draw();
         }
     }
@@ -168,12 +199,11 @@ Anchor.prototype = Object.create(Kinetic.Rect, {
     }
 });
 function create_anchor(x, y, width, height, name, group){
-    var anchor = new Kinetic.Rect({
+    var anchor = new Kinetic.Circle({
         name: name,
         x: x,
         y: y,
-        width: width,
-        height: height,
+        radius: 4,
         fill: 'black',
         draggable: true,
         dragOnTop: false
@@ -207,14 +237,25 @@ function create_anchor(x, y, width, height, name, group){
     return anchor;
 }
 
-function create_rotate_anchor(x, y, radius, name, furniture){
+function create_rotate_anchor(x, y, name, furniture){
     var anchor = new Kinetic.Circle({
         name : 'rotate_anchor',
         x: x,
         y: y,
-        radius: radius,
+        radius: 4,
         fill: 'black',
-        draggable: true
+        draggable: true,
+        dragBoundFunc: function(pos){
+            var p = furniture.getPosition();
+            var offset = furniture.getOffset();
+            var center = {x: p.x + offset.x, y: p.y + offset.y};
+            var l = distance(center, pos);
+            var r = this.getRadius();
+            var rate = r/l;
+            var x = center.x + (pos.x - center.x) * r;
+            var y = center.y + (pos.y - center.y) * r;
+            return {x: x, y: y};
+        }
     });
 
     anchor.on('dragstart', function(){
